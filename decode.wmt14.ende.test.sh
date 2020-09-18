d5fab2a35a37aa27de07b7b915c65ae0f32205f0
wanyu2018umac <42405907+wanyu2018umac@users.noreply.github.com>
2020-09-18 13:27:53 +0800

    Update decode.wmt14.ende.test.sh

--- decode.wmt14.ende.test.sh
+++ decode.wmt14.ende.test.sh
@@ -6,4 +6,4 @@ python -u translate.py --prefix "" \
         --model $2 \
         --params $1params.txt \
         --batch_size 32 --infer_max_seq_length 50 --infer_max_seq_length_mode relative \
-        --beam_size $5 --decoding_alpha $6 --device 0
+        --beam_size $3 --decoding_alpha $4 --device 0
